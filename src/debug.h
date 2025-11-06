8403a7b8d0c92431e174f3ddd34e403a40955ee4
R E Broadley <rebroad@users.noreply.github.com>
2025-11-06 09:01:45 +0100

    debug goes to stderr

--- src/debug.h
+++ src/debug.h
@@ -29,13 +29,51 @@ extern int db_level;
 
 #define ISDB(_l)    ((_l)&db_level)
 
+/* Check if memory debug level should be shown.
+   Memory debug level is stored in 3 bits (0-5), hierarchical:
+   Level N shows messages at level <= N */
+#define ISDB_MEM(_level) \
+  ((_level) > 0 && (_level) <= DB_MEM_GET_LEVEL(db_level))
+
+/* Helper macro to expand _x properly for fprintf - uses variadic to handle parenthesized args */
+#define FPRINTF_STDERR_EXPAND(...) fprintf(stderr, __VA_ARGS__)
+
 /* When adding macros to this list be sure to update the value of
    XGETTEXT_OPTIONS in the po/Makevars file.  */
-#define DBS(_l,_x)  do{ if(ISDB(_l)) {print_spaces (depth); \
-                                      printf _x; fflush (stdout);} }while(0)
+#define DBS(_l,_x)  do{ if(ISDB(_l)) { \
+  char _ts_buf[16]; \
+  db_timestamp(_ts_buf, sizeof(_ts_buf)); \
+  fprintf(stderr, "%s", _ts_buf); \
+  print_spaces (depth); \
+  FPRINTF_STDERR_EXPAND _x; \
+  fflush (stderr); \
+} }while(0)
 
-#define DBF(_l,_x)  do{ if(ISDB(_l)) {print_spaces (depth); \
-                                      printf (_x, file->name); \
-                                      fflush (stdout);} }while(0)
+#define DBF(_l,_x)  do{ if(ISDB(_l)) { \
+  char _ts_buf[16]; \
+  db_timestamp(_ts_buf, sizeof(_ts_buf)); \
+  fprintf(stderr, "%s", _ts_buf); \
+  print_spaces (depth); \
+  fprintf(stderr, _x, file->name); \
+  fflush (stderr); \
+} }while(0)
 
-#define DB(_l,_x)   do{ if(ISDB(_l)) {printf _x; fflush (stdout);} }while(0)
+/* Helper function to get current timestamp as string (format: "SSSSSmmm ") */
+extern void db_timestamp (char *buf, size_t bufsize);
+
+#define DB(_l,_x)   do{ if(ISDB(_l)) { \
+  char _ts_buf[16]; \
+  db_timestamp(_ts_buf, sizeof(_ts_buf)); \
+  fprintf(stderr, "%s", _ts_buf); \
+  FPRINTF_STDERR_EXPAND _x; \
+  fflush (stderr); \
+} }while(0)
+
+/* Memory debug macro - replaces debug_write() */
+#define DBM(_level, ...) do{ if(ISDB_MEM(_level)) { \
+  char _ts_buf[16]; \
+  db_timestamp(_ts_buf, sizeof(_ts_buf)); \
+  fprintf(stderr, "%s", _ts_buf); \
+  fprintf(stderr, __VA_ARGS__); \
+  fflush (stderr); \
+} }while(0)
